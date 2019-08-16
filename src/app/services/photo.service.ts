f05fea92cfa293750a6fbd462d03bb9a329c12c4
unotovive <tec.unotovive@gmail.com>
2019-08-16 14:09:18 +0900

    first commit

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Camera } from '@ionic-native/camera/ngx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  public photos: Photo[] = [];
+
+  constructor(private camera: Camera) { }
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // Add new photo to gallery
+      this.photos.unshift({
+          data: 'data:image/jpeg;base64,' + imageData
+      }); }, (err) => {
+      // Handle error
+      console.log("Camera issue: " + err);
+  });
+  }
+}
+
+class Photo {
+  data: any;
+}
